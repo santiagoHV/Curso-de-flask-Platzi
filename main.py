1df58b31856555105da93608c5487d8964c2a3d3
santiagoHV <santiherreravelas@gmail.com>
2021-03-14 22:55:35 -0500

    Estructuras de control

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__) #Se instancia la aplicación
 
+to_dos = ['to_do1','to_do2','to_do3']
+
 @app.route('/')
 def index():
     user_ip = request.remote_addr #Obtiene de request la ip local
@@ -15,5 +17,9 @@ def index():
 @app.route('/hello') #Ruta en la que se correra la funcion
 def hello():
     user_ip = request.cookies.get('user_ip')
+    context = {
+        'user_ip': user_ip,
+        'to_dos': to_dos
+    } #diccionario con los valores a pasar al template
 
-    return render_template('hello.html', user_ip = user_ip)
+    return render_template('hello.html', **context) # ** expande el diccionario
