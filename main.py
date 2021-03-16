de1e11d01dda755125b728b281bc87a7ee48e7c3
santiagoHV <santiherreravelas@gmail.com>
2021-03-15 21:56:56 -0500

    Configurar paginas de error

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ app = Flask(__name__) #Se instancia la aplicación
 
 to_dos = ['Comprar pan','Pagar la luz','Dormir']
 
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error = error)
+
 @app.route('/')
 def index():
     user_ip = request.remote_addr #Obtiene de request la ip local
