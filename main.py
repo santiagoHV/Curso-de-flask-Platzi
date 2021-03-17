b1ead6a389a45dccb86b725e9e3b83f9afacd67d
santiagoHV <santiherreravelas@gmail.com>
2021-03-17 11:04:37 -0500

    Configuracion flask

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__) #Se instancia la aplicación
 bootstrap = Bootstrap(app)# se instancia bootstrap
+
+app.config['SECRET_KEY'] = 'SUPER SECRETO' #Se configura una llave secreta
 
 
 
@@ -22,14 +24,15 @@ def index():
     user_ip = request.remote_addr #Obtiene de request la ip local
 
     response = make_response(redirect('/hello')) #crea una redireccion usando response a la ruta especificada
-    response.set_cookie('user_ip', user_ip) #Crea una cookie y manda la ip en string
+
+    session['user_ip'] = user_ip #Se manda el valor por sesion
 
     return response
 
 
 @app.route('/hello') #Ruta en la que se correra la funcion
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
     context = {
         'user_ip': user_ip,
         'to_dos': to_dos
---
+++ turn_on_server.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+pip install -r requirements.txt
+
+set FLASK_APP=main.py
+set FLASK_DEBUG=1
+set FLASK_ENV=development
+
+
