b09376cc26896a9e9e0e37623e5577e883a60df6
santiagoHV <santiherreravelas@gmail.com>
2021-03-17 18:23:47 -0500

    Testing y flashes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, make_response, redirect, render_template, session
+from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
 from Login import LoginForm
+import unittest
 
 app = Flask(__name__) #Se instancia la aplicación
 bootstrap = Bootstrap(app)# se instancia bootstrap
@@ -10,6 +11,12 @@ app.config['SECRET_KEY'] = 'SUPER SECRETO' #Se configura una llave secreta
 
 
 to_dos = ['Comprar pan','Pagar la luz','Dormir']
+
+@app.cli.command() #Hace que el nombre de la funcion sea un comando
+def test():
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner().run(tests)
+
 
 @app.errorhandler(404)
 def not_found(error):
@@ -28,17 +35,29 @@ def index():
 
     session['user_ip'] = user_ip #Se manda el valor por sesion
 
+
     return response
 
 
-@app.route('/hello') #Ruta en la que se correra la funcion
+@app.route('/hello', methods=['GET', 'POST']) #Ruta en la que se correra la funcion
 def hello():
     user_ip = session.get('user_ip')
     login_form = LoginForm()
+    username = session.get('username')
+
     context = {
         'user_ip': user_ip,
         'to_dos': to_dos,
-        'login_form': login_form
+        'login_form': login_form,
+        'username': username
     } #diccionario con los valores a pasar al template
+
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+
+        flash('Nombre de usuario registrado con exito') #Manda un mensaje tipo alert
+
+        return redirect(url_for('index'))
 
     return render_template('hello.html', **context) # ** expande el diccionario
